56bfdf99e48015a92d68277f890197b826dbddf1
Vadim Apisov <wvdim0@gmail.com>
2020-12-26 05:36:59 +0300

    Initial Commit

---
+++ FavPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FavPlaces
+//
+//  Created by Вадим Аписов on 26.12.2020.
+//  Copyright © 2020 Вадим Аписов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
