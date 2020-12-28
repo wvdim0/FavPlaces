3d7c1a761509258b937b14b052f50829eeb2c084
Vadim Apisov <wvdim0@gmail.com>
2020-12-28 05:23:27 +0300

    CustomTableViewCell

---
+++ FavPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  FavPlaces
+//
+//  Created by Вадим Аписов on 28.12.2020.
+//  Copyright © 2020 Вадим Аписов. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+
+}
--- FavPlaces/MainViewController.swift
+++ FavPlaces/MainViewController.swift
@@ -29,12 +29,13 @@ class MainViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
 
-        cell.textLabel?.text = restNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: restNames[indexPath.row])
-        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
-        cell.imageView?.clipsToBounds = true
+        cell.nameLabel.text = restNames[indexPath.row]
+        cell.imageOfPlace.image = UIImage(named: restNames[indexPath.row])
+//        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+        cell.imageOfPlace.layer.cornerRadius = 12
+        cell.imageOfPlace.clipsToBounds = true
         
         return cell
     }
